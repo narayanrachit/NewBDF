c1604da04f2b572334fd72880c7e6316c5f2191a
narayanrachit <narayanrachit@gmail.com>
2020-07-20 22:06:48 +0530

    R.B header added

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-$(window).scroll(function () {
-    if ( $(window).scrollTop() >= 690 && $(window).scrollTop() <= 740) {
-    $("#menu").css('background-image','linear-gradient(to right, #cc9966 , white)');
-    } else {
-    $("#menu").css('background-color','transparent');
-    $("#menu").css('background-image','none');
-    }
-    });
+// $(window).scroll(function () {
+//     if ( $(window).scrollTop() >= 690 && $(window).scrollTop() <= 740) {
+//     $("#menu").css('background-image','linear-gradient(to right, #cc9966 , white)');
+//     } else {
+//     $("#menu").css('background-color','transparent');
+//     $("#menu").css('background-image','none');
+//     }
+//     });
 
     //linear-gradient(to right, #cc9966 , white)
